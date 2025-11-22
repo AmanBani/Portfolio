9549382a8cd006ce739dad4ffd309d55e9b6ca9c
Aman Bani <amanbani647@gmail.com>
2025-11-22 23:47:15 +0530

    addes contact and projects page

---
+++ app/Contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This Page contains my Contact information and a Form to connect and send their message.</div>
+  )
+}
+
+export default page
