a065a54440ca9e37c6009b7b07fb40f2bf3c7668
Aman Bani <amanbani647@gmail.com>
2025-11-22 23:59:16 +0530

    added Blogs page

---
+++ app/Blogs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This page contains my experice of Projects and Blogs Aboit the tech i am learnign and had expericen .</div>
+  )
+}
+
+export default page
