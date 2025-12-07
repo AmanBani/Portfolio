b14da5aae9699934b84e8272269bef00ff924f2c
Aman Bani <amanbani647@gmail.com>
2025-12-07 22:59:26 +0530

    frontend chages 01

--- app/Blogs/page.tsx
+++ app/Blogs/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
+    <>
     <div>This page contains my experice of Projects and Blogs Aboit the tech i am learnign and had expericen .</div>
+
+    <div>This page contains my experice of Projects and Blogs Aboit the tech i am learnign and had expericen .</div>
+
+    <div>This page contains my experice of Projects and Blogs Aboit the tech i am learnign and had expericen .</div>
+  </>
   )
 }
 
