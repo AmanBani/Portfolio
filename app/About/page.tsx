182f5ad13e2d9b1b03d760a47a4f3876a2b15344
Aman Bani <amanbani647@gmail.com>
2025-12-07 23:00:11 +0530

    frontend changes 02

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
+    <>
     <div>This page holds the most of the Things About me as person.</div>
+    <div>This page holds the most of the Things About me as person.</div>
+    <div>This page holds the most of the Things About me as person.</div>
+    <div>This page holds the most of the Things About me as person.</div>
+
+    </>
   )
 }
 
